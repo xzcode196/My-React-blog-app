5c5b1699f956d41668ace7c8c2ac1f138524ca09
Xiang <xzhang48@charlotte.edu>
2026-04-09 21:56:48 -0400

    Updated import for App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 
 import Header from "./Components/common/Header";
